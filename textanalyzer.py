88aa9b1ed8017c0f9d21d8400d8d8fd36fd1a366
gssantos02 <137155659+gssantos02@users.noreply.github.com>
2023-06-20 16:12:34 +0800

    Update textanalyzer.py

--- textanalyzer.py
+++ textanalyzer.py
@@ -4,7 +4,10 @@ import pandas as pd
 st.title("Ticket Classifier")
 
 st.header("Upload Ticket Data")
-uploaded_file = st.file_uploader("Upload a .csv file")
 
-df = pd.read_csv(uploaded_file)
-st.write(df.head(5))
+with st.form("Uplod File"):
+  uploaded_file = st.file_uploader("Upload a .csv file")
+  upload = st.form_submit_button("Upload")
+  if submit:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head(5))
