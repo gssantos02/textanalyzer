8339133a219af22882960c2b8dee35d5f3468001
gssantos02 <137155659+gssantos02@users.noreply.github.com>
2023-06-20 16:58:59 +0800

    Update textanalyzer.py

--- textanalyzer.py
+++ textanalyzer.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
+from transformers import pipeline
+
+classifier = pipeline("zero-shot-classification")
 
 st.title("Ticket Classifier")
-
 st.header("Upload Ticket Data")
 
 uploaded_file = st.file_uploader("Upload a .csv file")
@@ -10,3 +12,16 @@ if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
   df = df["Title"]
   st.write(df)
+  
+with st.form("Sample Description"):
+   prompt = st.text_area("Prompt", "Input sample ticket description")
+   submit = st.form_submit_button("Submit")
+  if submit is not None:
+    res = classifier(
+      prompt,
+      candidate_labels=['access','hardware','software'],
+    )
+    res = res['labels'][0]
+    st.write(res)
+
+
