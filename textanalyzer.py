8504fa07278bb3492414c0abd2a643c30d8956c2
gssantos02 <137155659+gssantos02@users.noreply.github.com>
2023-06-20 16:34:53 +0800

    Update textanalyzer.py

--- textanalyzer.py
+++ textanalyzer.py
@@ -8,5 +8,5 @@ st.header("Upload Ticket Data")
 uploaded_file = st.file_uploader("Upload a .csv file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  df = df.head(5)
+  df = df["Title"]
   st.write(df)
