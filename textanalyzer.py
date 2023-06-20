dc16e58e537b52996d60034029be2f98652ea1d2
gssantos02 <137155659+gssantos02@users.noreply.github.com>
2023-06-20 16:15:45 +0800

    Update textanalyzer.py

--- textanalyzer.py
+++ textanalyzer.py
@@ -8,6 +8,6 @@ st.header("Upload Ticket Data")
 with st.form("Uplod File"):
   uploaded_file = st.file_uploader("Upload a .csv file")
   upload = st.form_submit_button("Upload")
-  if submit:
+  if upload:
     df = pd.read_csv(uploaded_file)
     #st.write(df.head(5))
