72cd813d1b2d1252fca2963eb5456e79d4435497
gssantos02 <137155659+gssantos02@users.noreply.github.com>
2023-06-20 16:06:16 +0800

    Update textanalyzer.py

--- textanalyzer.py
+++ textanalyzer.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 
 st.title("Ticket Classifier")
-st.info("Group tickets based on their description")
 
 st.header("Upload Ticket Data")
-uploaded_file = st.fileuploader("Upload a .csv file")
+uploaded_file = st.file_uploader("Upload a .csv file")
