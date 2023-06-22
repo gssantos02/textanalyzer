9292c17f75a8f4a505d2675e7e7eaa599fb1299a
gssantos02 <137155659+gssantos02@users.noreply.github.com>
2023-06-22 13:41:52 +0800

    Update textanalyzer.py

--- textanalyzer.py
+++ textanalyzer.py
@@ -6,18 +6,17 @@ from transformers import pipeline
 st.title("Ticket Classifier")
 st.header("Upload Ticket Data")
 
-uploaded_file = st.file_uploader("Upload a .csv file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  df = df["Title"]
-  st.write(df)
-  
+#uploaded_file = st.file_uploader("Upload a .csv file")
+#if uploaded_file is not None:
+  #df = pd.read_csv(uploaded_file)
+  #df = df["Title"]
+  #st.write(df)
+
+
 with st.form("Sample Description"):
    prompt = st.text_area("Prompt", "")
    submit = st.form_submit_button("Submit")
-#if submit:
-  #st.write("Functioning correctly")
-#st.cache(allow_output_mutation=True)
+
 with st.spinner('Analyzing...'):
     if submit:
       classifier = pipeline(model="facebook/bart-large-mnli")
