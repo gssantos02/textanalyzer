8c3e84b0a05ed25b768a9e1c19c4c344a41b9e54
gssantos02 <137155659+gssantos02@users.noreply.github.com>
2023-06-22 11:13:42 +0800

    Defined a model and add spinner

--- textanalyzer.py
+++ textanalyzer.py
@@ -2,11 +2,6 @@ import streamlit as st
 import pandas as pd
 from transformers import pipeline
 
-##@st.cache(allow_output_mutation=True)
-#def load_model(model_name):
-  #classifier = pipeline(model_name)
-  #return(classifier)
-#classifier = load_model("zero-shot-classification")
 
 st.title("Ticket Classifier")
 st.header("Upload Ticket Data")
@@ -18,17 +13,16 @@ if uploaded_file is not None:
   st.write(df)
   
 with st.form("Sample Description"):
-   prompt = st.text_area("Prompt", "Sample Ticket Description")
-   submit = st.form_submit_button("Generate Category")
-if submit:
-  st.write("Functioning correctly")
-#if submit is not None:
-  #classifier = pipeline("zero-shot-classification")
-  #res = classifier(
-    #prompt,
-    #candidate_labels=['access','hardware','software'],
-  #)
-  #tag = res['labels'][0]
-  #st.write(tag)
-
-
+   prompt = st.text_area("Prompt", "")
+   submit = st.form_submit_button("Submit")
+#if submit:
+  #st.write("Functioning correctly")
+with st.spinner('Analyzing...'):
+    if submit:
+      classifier = pipeline(model="facebook/bart-large-mnli")
+      res = classifier(
+        prompt,
+        candidate_labels=['access','hardware','software'],
+      )
+      tag = res['labels'][0]
+      st.write(tag)
